aaa7944dba02d41a1c4356a34b0ce884e05f74b8
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-07 16:16:27 +0100

    Start defining API endpoints.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express, app, env, audioMiddleware, port;
 
 express = require('express');
 app = express();
+audioMiddleware = require('./server/middleware/audio');
 
 // Create a static file server
 // app.configure() is depreacted, c.f. https://github.com/strongloop/express/wiki/Migrating-from-3.x-to-4.x#appconfigure
@@ -10,8 +11,18 @@ if ('development' === env) {
   app.use(express.static(__dirname + '/public'));
 };
 
-audioMiddleware = require('./server/middleware/audio');
-audioMiddleware.loop();
+app.route('/api')
+    .get(function(req, res) {
+        res.send('Fetch an object');
+    })
+    .post(function(req, res) {
+        res.send('Create a new object');
+    })
+    .put(function(req, res) {
+        res.send('Update an object');
+    });
+
+// audioMiddleware.loop();
 
 port = 8080;
 app.listen(port);
