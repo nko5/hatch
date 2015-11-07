39019ae0cf5a8466a8cea0e08bba593749c738c7
Richard Armuelles <alexanderarmuelles@hotmail.com>
2015-11-07 01:47:24 -0500

    I create a test json as example

--- app.js
+++ app.js
@@ -12,14 +12,34 @@ if ('development' === env) {
 };
 
 // Get the dummy data
-//var test = require('./server/ddata.json');
-//var parse = JSON.parse(test);
+//var test = require("./server/ddata.js")
+var test = '{"pixel": [{"index":"1","r":"190","g":"85","b":"130","a":"0.5"}], "pixel": [{"index":"2","r":"110","g":"45","b":"90","a":"0.9"}]}';
+var parse = JSON.parse(test);
 
-var n = 0;
+var i = 0;
+
+function loop() {
+  setTimeout(function(){
+
+    var b = baudio(function (t) {
+        return Math.sin(t * parse.pixel[i].r * Math.PI * parse.pixel[i].g) +
+        Math.sin(t * parse.pixel[i].b) * (t % parse.pixel[i].index > parse.pixel[i].a);
+    });
+    b.play();
+
+    if(i < 10){
+      loop();
+    }
+
+  }, 3000);
+}
+
 var b = baudio(function (t) {
-    return Math.sin(t * 400 * Math.PI * 2) + Math.sin(t * 500) * (t % 2 > 1);
+    return Math.sin(t * parse.pixel[i].r * Math.PI * parse.pixel[i].g) +
+    Math.sin(t * parse.pixel[i].b) * (t % parse.pixel[i].index > parse.pixel[i].a);
 });
 b.play();
+
 
 port = 8080;
 app.listen(port);
