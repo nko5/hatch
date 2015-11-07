b0eb3605b19a981bc76107334ed753dd27bfbe20
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-07 03:28:48 +0100

    Use newer express syntax for routing.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var app = express();
+var express, app, env, port;
 
-//Create a static file server
-app.configure(function() {
+express = require('express');
+app = express();
+
+// Create a static file server
+// app.configure() is depreacted, c.f. https://github.com/strongloop/express/wiki/Migrating-from-3.x-to-4.x#appconfigure
+env = process.env.NODE_ENV || 'development';
+if ('development' === env) {
   app.use(express.static(__dirname + '/public'));
-});
+};
 
-//Get the dummy data
+// Get the dummy data
 require('./server/ddata.js');
 
-var port = 8080;
+port = 8080;
 app.listen(port);
 console.log('Express server started on port %s', port);
