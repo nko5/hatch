3bca4b77fe45cf5bb6ce408267ea7883558b5458
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-07 04:26:31 +0100

    previewImage on file "upload"

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,9 +1,35 @@
 define(function() {
     "use strict";
-    var upload, canvas;
+    var upload, canvas, previewImage;
 
     upload = document.getElementById("upload");
     canvas = document.getElementById("canvas");
-    console.log("Canvas loaded", upload, canvas);
-    return {}
+
+    previewImage = function() {
+        var file, ctx, reader, img;
+
+        file = upload.files[0];
+        ctx = canvas.getContext("2d");
+        reader = new FileReader();
+        console.log("previewImage", file, ctx, reader);
+
+        reader.onloadend = function() {
+            img = new Image();
+            img.onload = function() {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+            };
+            // Triggers load event on img
+            img.src = reader.result;
+        };
+
+        // Triggers loadend event on reader
+        reader.readAsDataURL(file);
+    };
+
+    upload.addEventListener('change', previewImage);
+    return {
+        previewImage: previewImage
+    }
 });
