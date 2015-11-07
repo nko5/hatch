fc0892435239ee676b0aa2031dc62c8fb9bf240d
Richard Armuelles <alexanderarmuelles@hotmail.com>
2015-11-07 07:52:20 -0500

    trying to solve the problem with test json

--- server/middleware/audio/core.js
+++ server/middleware/audio/core.js
@@ -1,7 +1,7 @@
 (function(audioGlobal) {
     "use strict";
     var baudio, makeApi, isNode, isAMD, loadDependencies;
-    
+
     baudio = require('baudio');
 
     makeApi = function(audio) {
@@ -9,30 +9,24 @@
         var test, parse, i, b;
 
         // Get the dummy data
-        test = '{"pixel": [{"index":"1","r":"190","g":"85","b":"130","a":"0.5"}], "pixel": [{"index":"2","r":"110","g":"45","b":"90","a":"0.9"}]}';
+        test = '[{"index":"1","r":"190","g":"85","b":"130","a":"0.5"}, {"index":"2","r":"110","g":"45","b":"90","a":"0.9"}]';
         parse = JSON.parse(test);
 
         i = 0;
 
-        b = baudio(function (t) {
-            return Math.sin(t * parse.pixel[i].r * Math.PI * parse.pixel[i].g) +
-                Math.sin(t * parse.pixel[i].b) * (t % parse.pixel[i].index > parse.pixel[i].a);
-        });
-        b.play();
-
         audio.loop = function() {
             setTimeout(function(){
                 var b = baudio(function (t) {
-                    return Math.sin(t * parse.pixel[i].r * Math.PI * parse.pixel[i].g) +
-                        Math.sin(t * parse.pixel[i].b) * (t % parse.pixel[i].index > parse.pixel[i].a);
+                    return Math.sin(t * parse[i].r * Math.PI * parse[i].g) +
+                        Math.sin(t * parse[i].b) * (t % parse[i].index > parse[i].a);
                 });
                 b.play();
-
-                if(i <= 10){
+                i++;
+                if(i <= test.length-1){
                     audio.loop();
                 }
 
-            }, 5000);
+            }, 3000);
         };
     };
 
