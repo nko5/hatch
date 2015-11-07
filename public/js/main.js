32a4d1745bb81c1b7a6b1fc7be9d9809d892c715
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-07 06:47:37 +0100

    Use q-xhr as AJAX library.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
 // Main entry point of the app. Load dependencies here
+// paths property will work as fallback route, if a dependency could not be found in baseUrl
+requirejs.config({
+    paths: {
+        q: [
+            "lib/q"
+        ],
+        "q-xhr": [
+            "lib/q-xhr"
+        ]
+    }
+});
+
 require(["canvas"], function(canvas) {
     "use strict";
     console.log("Canvas module loaded", canvas);
