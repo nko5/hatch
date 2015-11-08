5d0ebb89d1b2826495fa4c4a9e47b2f4637b649d
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-08 22:32:23 +0100

    Socket IO lib not found on client.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,12 @@
 // Main entry point of the app. Load dependencies here
 // paths property will work as fallback route, if a dependency could not be found in baseUrl
-socketDomain = window.location.protocol + "//" + window.location.hostname + ":4343";
-
 requirejs.config({
     paths: {
         q: [
             "../components/q/q"
         ],
         socketio: [
-            socketDomain + "/components/socket/socket-io"
+            "../components/socket/socket-io"
         ],
         "q-xhr": [
             "../components/q-xhr/q-xhr"
