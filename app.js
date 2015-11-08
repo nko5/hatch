f9effdbe4f1a4e0254c81ed0970156ca9410c933
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-08 22:39:49 +0100

    CORS

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ bodyParser = require('body-parser');
 audioMiddleware = require('./server/middleware/audio');
 
 app.use(cors());
-app.use(function(req, res, next) {
+app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     next();
 });
 
