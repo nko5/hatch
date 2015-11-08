d5bf505659e75a60af1dda3d99d4669dbcecdd77
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-08 22:29:04 +0100

    CORS

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-var express, app, server, io, bodyParser, audioMiddleware, env, port;
+var express, cors, app, server, io, bodyParser, audioMiddleware, env, port;
 
 express = require('express');
+cors = require('cors');
 app = express();
 server = require('http').createServer(app);
 io = require('socket.io')(server);
 bodyParser = require('body-parser');
 audioMiddleware = require('./server/middleware/audio');
+
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
 // Create a static file server
 // app.configure() is depreacted, c.f. https://github.com/strongloop/express/wiki/Migrating-from-3.x-to-4.x#appconfigure
@@ -40,6 +50,8 @@ app.route('/api')
 port = 8080;
 app.listen(port);
 
+// io.set("origins", "*");
+// io.set("transports", ["polling", "websocket", "xhr-polling"]);
 io.on('connection', function() {
     console.log('Hello, socket.io!');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,14 @@
 // Main entry point of the app. Load dependencies here
 // paths property will work as fallback route, if a dependency could not be found in baseUrl
+socketDomain = window.location.protocol + "//" + window.location.hostname + ":4343";
+
 requirejs.config({
     paths: {
         q: [
             "../components/q/q"
         ],
         socketio: [
-            "../components/socket/socket-io"
+            socketDomain + "/components/socket/socket-io"
         ],
         "q-xhr": [
             "../components/q-xhr/q-xhr"
