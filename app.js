1a3ce6064afbb13074557895ee7724963e07fde6
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-11-09 00:35:31 +0100

    Discard Socket.IO

--- app.js
+++ app.js
@@ -8,14 +8,19 @@ io = require('socket.io').listen(server);
 bodyParser = require('body-parser');
 audioMiddleware = require('./server/middleware/audio');
 
-app.use(cors());
-app.use(function(req, res, next) {
-    console.log("Query for socket.io");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// enable if you have problems with Socket.IO but I cannot promise anything.
+RUNNING_SOCKETIO = false
 
+if (RUNNING_SOCKETIO) {
+    app.use(cors());
+    app.use(function(req, res, next) {
+        console.log("Query for socket.io", process.env);
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+}
+    
 
 // Create a static file server
 // app.configure() is depreacted, c.f. https://github.com/strongloop/express/wiki/Migrating-from-3.x-to-4.x#appconfigure
@@ -49,13 +54,16 @@ app.route('/api')
 
 port = 8080;
 app.listen(port);
-socketPort = 4343;
 
-// io.set("origins", "*");
-// io.set("transports", ["polling", "websocket", "xhr-polling"]);
-io.on('connection', function() {
-    console.log('Hello, socket.io!');
-});
-server.listen(socketPort);
-console.log('Socket server started on port %s', socketPort);
+if (RUNNING_SOCKETIO) {
+    socketPort = 4343;
+
+    // io.set("origins", "*");
+    // io.set("transports", ["polling", "websocket", "xhr-polling"]);
+    io.on('connection', function() {
+        console.log('Hello, socket.io!');
+    });
+    server.listen(socketPort);
+    console.log('Socket server started on port %s', socketPort);
+}
 console.log('Express server started on port %s', port);
